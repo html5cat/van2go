26f4bd86b3ee0d438ef75acae477aad624ed41af
Yuriy Dybskiy <yuriy@dybskiy.com>
2012-09-17 12:28:55 -0700

    Add Facebook authentication

---
+++ server/secrets.js
@@ -0,0 +1,4 @@
+
+Meteor.accounts.facebook.setSecret('75d703c26828df158575071e812e86b8');
+
+// Meteor.accounts.google.setSecret('SECRET');
--- van2go.js
+++ van2go.js
@@ -1,3 +1,7 @@
 Locations = new Meteor.Collection("locations");
 
 Cars = new Meteor.Collection("cars");
+
+// Meteor.accounts.facebook.config('343994189024033', 'http://van2go.meteor.com');
+
+Meteor.accounts.facebook.config('343994189024033', 'http://localhost:3000');
