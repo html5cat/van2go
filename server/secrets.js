e8f5f8847985100b576bee097961ca2a788d5ff8
Yuriy Dybskiy <yuriy@dybskiy.com>
2013-12-23 14:27:51 -0800

    Remove Facebook and Madewith

--- server/secrets.js
+++
@@ -1,4 +0,0 @@
-
-Meteor.accounts.facebook.setSecret('75d703c26828df158575071e812e86b8');
-
-// Meteor.accounts.google.setSecret('SECRET');
--- van2go.js
+++ van2go.js
@@ -1,7 +1,3 @@
 Locations = new Meteor.Collection("locations");
 
 Cars = new Meteor.Collection("cars");
-
-// Meteor.accounts.facebook.config('343994189024033', 'http://van2go.meteor.com');
-
-Meteor.accounts.facebook.config('343994189024033', 'http://localhost:3000');
